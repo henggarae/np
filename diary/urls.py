18cacb0a93e30c3d8a9fc1d055a3bb539f4fddd6
admin <rnseodydrPwjd1@gmail.com>
2021-08-07 02:24:45 +0000

    urlpatterns

---
+++ diary/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import view
+urlpatterns = [
+#    path('diary/', view.page_list),
+#    path('diary/info/', view.page_info),
+#    path('diary/write/', view.page_create),
+#    path('diary/page/<int:id>', view.page_detail),
+#    path('diary/page/<int:id>/edit', view.page_update),
+#    path('diary/page/<int:id>/delete', view.page_delete),
+
+]

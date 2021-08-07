680c77040c7185e9f8b0193541e3370b03a51c0e
admin <rnseodydrPwjd1@gmail.com>
2021-08-07 10:02:17 +0000

    connect url

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Page
 
-# Create your views here.
+def page_list(request):
+    posts = Page.objects.all()
+    context = {"posts":posts}
+    
+    return render(request, 'diary/page_list.html', context = context)
+
+def page_detail(request, pg_id):
+    post = Page.objects.get(id=pg_id)
+    context = {"post":post}
+    
+    return render(request, "diary/page_detail.html", context = context)
+
+
+
